e49a1a79bc744be794baa843fd209272d4eaa240
Vladimir Milinic <vladimir.milinic@gmail.com>
2026-06-10 23:39:48 +0200

    build basic layout and styling for projects section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from '@/components/home/Hero';
 import About from '@/components/home/About';
+import Projects from '@/components/home/Projects';
 
 export default function Home() {
    return (
       <>
          <Hero />
          <About />
+         <Projects />
       </>
    );
 }
